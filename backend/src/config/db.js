c71b2645d49ab04ea9b4ef01fd3153ef35552b08
Joaquin Ledezma Barragan <j.ledezmab0.2@gmail.com>
2026-04-06 19:51:16 -0600

    cambios para deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,24 +1,17 @@
-// ==============================
-// database.js / connectDB.js
-// Conexión a MongoDB (Mongoose)
-// ==============================
-
 const mongoose = require("mongoose");
 
-// ==============================
-// Conectar a MongoDB
-// ==============================
 const connectDB = async () => {
   try {
-    // Usa la URI desde variables de entorno
+    // 🔍 DEBUG (IMPORTANTE)
+    console.log("MONGO_URI debug:", JSON.stringify(process.env.MONGO_URI));
+    console.log("MONGO_URI tipo:", typeof process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB conectado correctamente");
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB");
     console.error(error.message);
-
-    // Detiene la app si no hay DB
     process.exit(1);
   }
 };
